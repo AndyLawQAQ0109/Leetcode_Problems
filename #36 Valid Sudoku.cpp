f7e7f647f55555299d91fc9ab6cb1ceac00c13f6
AndyLawQAQ0109 <70401273+AndyLawQAQ0109@users.noreply.github.com>
2022-06-06 23:44:04 -0700

    Update #36 Valid Sudoku.cpp

--- #36 Valid Sudoku.cpp
+++ #36 Valid Sudoku.cpp
@@ -20,3 +20,27 @@ public:
         return true;
     }
 };
+
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        int row[9][10] = {0};
+        int column[9][10] = {0};
+        int grid[9][10] = {0};
+        for(int i=0; i < 9; i++){           
+            for(int j=0; j < 9; j++){
+                int c = board[i][j] - '0';
+                if(board[i][j] != '.'){                   
+                    if(row[i][c] || column[j][c] || grid[(i/3)*3+j/3][c])
+                        return false;
+
+                    row[i][c] = 1;
+                    column[j][c] = 1;
+                    grid[(i/3)*3+j/3][c] = 1;
+                }
+            }
+        }
+
+        return true;
+    }
+};
