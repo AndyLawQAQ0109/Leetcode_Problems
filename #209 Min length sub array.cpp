4803ed8b6577bfb18a254eadf13f3525331022e2
AndyLawQAQ0109 <70401273+AndyLawQAQ0109@users.noreply.github.com>
2022-06-19 15:41:35 -0700

    Update #209 Min length sub array.cpp

--- #209 Min length sub array.cpp
+++ #209 Min length sub array.cpp
@@ -25,3 +25,27 @@ public:
         return 0;
     }
 };
+
+
+// Shorter
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {      
+        int l = 0, r = 0;
+        int total = 0;
+        int res = INT_MAX;
+
+        while(r < nums.size()){        
+            total += nums[r];            
+            while(total >= target){
+                res = (r - l + 1) < res ? (r - l + 1) : res;
+                total -= nums[l];
+                l++;
+            }
+            r++;           
+        }
+        if(res < INT_MAX)
+            return res;
+        return 0;
+    }
+};
