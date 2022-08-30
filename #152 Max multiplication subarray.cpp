e39cda3a2da18fa4a12337bb3e356567da3bb29a
AndyLawQAQ0109 <70401273+AndyLawQAQ0109@users.noreply.github.com>
2022-08-30 01:11:27 -0700

    Create #152 Max multiplication subarray.cpp

---
+++ #152 Max multiplication subarray.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dpmin(n, 0);
+        vector<int> dpmax(n, 0);
+        dpmax[0] = nums[0];
+        dpmin[0] = nums[0];
+        
+        for(int i=1; i<n; i++){
+            dpmin[i] = min(nums[i], min(nums[i] * dpmin[i-1], nums[i] * dpmax[i-1]));
+            dpmax[i] = max(nums[i], max(nums[i] * dpmin[i-1], nums[i] * dpmax[i-1]));
+        }
+        return *max_element(dpmax.begin(), dpmax.end());
+    }
+};
